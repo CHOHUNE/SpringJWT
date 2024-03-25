d1e82787596d322da221f2e30375622e1e73f8bf
CHOHUNE <yuureru@gmail.com>
2024-03-25 13:07:08 +0900

    DB 단 로직 구현 CustomeUserDetailService 와 서비스 로직이 수행한 데이터를 담을 dto 격인 CustomUserDetails 를 이용해 DB단 로직 구현. UserRepository 에 findByUsername 추가

--- src/main/java/com/example/springjwt/repository/UserRepository.java
+++ src/main/java/com/example/springjwt/repository/UserRepository.java
@@ -7,4 +7,10 @@ public interface UserRepository extends JpaRepository<UserEntity,Long> {
 
     Boolean existsByUsername(String username);
 
+    UserEntity findByUsername(String username);
+    // 유저테이블에서 해당 유저를 조회하는 쿼리 : 리턴 타입은 유저 엔티티
+    // JPA 작명 관례에 준수해서 findBy 객체명을 적어넣어 주면 알아서 JPADATA가  완성 해준다.
+    // 이제 위 메서드를 통해서 특정 유저를 조회할 수 있다.
+    // 서비스단에서 구현하면 되는데, CustomUserDetailService 클래스를 만들어서 내부에서 서비스 로직을 구현한다.
+
 }
